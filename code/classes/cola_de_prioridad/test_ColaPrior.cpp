2178b25c779d28efc0e247d2c550e366175d2a29
Nicolás Berríos Verboven <nbverboven@gmail.com>
2016-11-20 18:59:38 -0300

    tests cola de prioridad

---
+++ code/classes/cola_de_prioridad/test_ColaPrior.cpp
@@ -0,0 +1,220 @@
+#include <iostream>
+#include <fstream>
+#include "ColaPrior.h"
+#include "ColaPrior.cpp"
+#include "../../mini_test.h"
+
+using namespace std;
+using namespace aed2;
+
+/*
+ * 20/11/2016 - 16:10
+ * Pasa todos los tests que puse.
+ * No pierde memoria.
+ * 
+ * --Nico
+ */
+
+
+
+/*
+ * Esto es una una tupla de dos Nat que puse sólo para los casos de test. Tiene un constructor con parámetros
+ * y un constructor por copia para poder usarlo en la cola de prioridad (recordar que los elementos se agregan por 
+ * copia).
+ * Habría que ver si en el caso más general no hace falta definir la clase por separado.
+ *
+ * --Nico
+ */
+struct TuplaReLoca
+{
+    TuplaReLoca(const Nat& p, const Nat& s) : fst_(p), snd_(s) {}
+    TuplaReLoca(const TuplaReLoca& otra) { fst_ = otra.fst_, snd_ = otra.snd_; }
+
+    const Nat& first() const { return fst_; }
+    const Nat& second() const { return snd_; }
+
+    bool operator<(const TuplaReLoca& otra) const { return fst_ < otra.fst_ || snd_ < otra.snd_; }
+
+    bool operator==(const TuplaReLoca& otra) const { return fst_ == otra.fst_ && snd_ == otra.snd_; }
+
+    Nat fst_;
+    Nat snd_;
+};
+
+// Para mostrar las tuplas por pantalla
+std::ostream& operator<<(std::ostream& os, const TuplaReLoca& t) 
+{ os << '\n' << '<' << t.first() << ',' << t.second() << '>' << '\n'; }
+
+
+
+void test_constructor()
+{
+    ColaPrior<int> cola();
+    ColaPrior<TuplaReLoca> otra_cola();
+}
+
+
+void test_encolar()
+{
+    ColaPrior<int> cola;
+
+    cola.encolar(3);
+    cola.encolar(4);
+    cola.encolar(6);
+    cola.encolar(5);
+}
+
+
+void test_desencolar()
+{
+    ColaPrior<int> cola;
+
+    ASSERT_EQ(cola.tamanio(), 0);
+    cola.encolar(3);
+    cola.encolar(4);
+    cola.encolar(6);
+    cola.encolar(5);
+
+
+    cola.desencolar();
+    cola.desencolar();
+    cola.desencolar();
+    cola.desencolar();
+}
+
+
+void test_esVacia()
+{
+    TuplaReLoca t1(1, 2);
+    // cout << t1 << endl;
+    ColaPrior<TuplaReLoca> cola;
+    ColaPrior<TuplaReLoca> otra_cola;
+
+    cola.encolar(t1);
+
+    ASSERT(!cola.esVacia());
+    ASSERT(otra_cola.esVacia());
+    // cout << "Llegué hasta acá" << endl;
+}
+
+
+void test_tamanio()
+{
+    ColaPrior<int> c1;
+    ColaPrior<int> c2;
+
+    c2.encolar(1);
+
+    ASSERT_EQ(c1.tamanio(), 0);
+    ASSERT_EQ(c2.tamanio(), 1);
+
+    c2.desencolar();
+
+    ASSERT_EQ(c2.tamanio(), 0);
+}
+
+
+void test_proximo()
+{
+    ColaPrior<int> c1;
+
+    c1.encolar(9);
+    c1.encolar(4);
+    c1.encolar(1);
+    c1.encolar(6);
+    c1.encolar(7);
+    c1.encolar(3);
+    c1.encolar(5);
+    c1.encolar(8);
+
+    cout << '\n' << "Estado inicial: " << c1 << '\n';
+
+    ASSERT_EQ(c1.proximo(), 1);
+
+    c1.desencolar();
+
+    cout << "Desencolo por 1ra vez: " << c1 << '\n';
+
+    ASSERT_EQ(c1.proximo(), 3);
+
+    c1.desencolar();
+
+    cout << "Desencolo por 2da vez: " << c1 << '\n';
+
+    ASSERT_EQ(c1.proximo(), 4);
+
+    c1.desencolar();
+
+    cout << "Desencolo por 3ra vez: " << c1 << '\n';
+
+    ASSERT_EQ(c1.proximo(), 5);
+
+    c1.desencolar();
+
+    cout << "Desencolo por 4ta vez: " << c1 << '\n';
+
+    ASSERT_EQ(c1.proximo(), 6);
+
+    c1.desencolar();
+
+    cout << "Desencolo por 5ta vez: " << c1 << '\n';
+
+    ASSERT_EQ(c1.proximo(), 7);
+
+    c1.desencolar();
+
+    cout << "Desencolo por 6ta vez: " << c1 << '\n';
+
+    ASSERT_EQ(c1.proximo(), 8);
+
+    c1.desencolar();
+
+    cout << "Desencolo por 7ma vez: " << c1 << '\n';
+
+    ASSERT_EQ(c1.proximo(), 9);
+
+    c1.desencolar();
+
+    cout << "Desencolo por 8va vez: " << c1 << '\n';
+
+    ASSERT(c1.esVacia());
+}
+
+
+void test_borrar_elem_intermedio()
+{
+    ColaPrior<int> c1;
+
+    c1.encolar(9);
+    c1.encolar(4);
+    c1.encolar(1);
+    ColaPrior<int>::ItColaPrior it = c1.encolar(6);
+    c1.encolar(7);
+    c1.encolar(3);
+    c1.encolar(5);
+    c1.encolar(8);
+
+    cout << '\n' << "Estado inicial: " << c1 << '\n';
+    
+    ASSERT_EQ(c1.tamanio(), 8);
+
+    it.eliminarSiguiente();
+
+    cout << "Borro el 6: " << c1 << '\n';
+
+    ASSERT_EQ(c1.tamanio(), 7);
+}
+
+
+int main()
+{
+    RUN_TEST(test_constructor);
+    RUN_TEST(test_encolar);
+    RUN_TEST(test_desencolar);
+    RUN_TEST(test_esVacia);
+    RUN_TEST(test_tamanio);
+    RUN_TEST(test_proximo);
+    RUN_TEST(test_borrar_elem_intermedio);
+
+    return 0;
+}
