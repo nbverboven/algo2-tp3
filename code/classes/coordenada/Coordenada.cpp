82778b290cfb3483caf446c892d5b9535d287ed8
Nicolás Berríos Verboven <nbverboven@gmail.com>
2016-11-21 12:54:19 +0000

    Update Coordenada.cpp

--- code/classes/coordenada/Coordenada.cpp
+++ code/classes/coordenada/Coordenada.cpp
@@ -1,24 +1,25 @@
 #include "Coordenada.h"
+using namespace aed2;
 
 Coordenada::Coordenada(const Nat x, const Nat y) : latitud(x), longitud(y)
-{
+{}
 
-}
 
 Coordenada::~Coordenada()
-{
+{}
 
-}
 
 const Nat Coordenada::Latitud() const
 {
     return this->latitud;
 }
 
+
 const Nat Coordenada::Longitud() const
 {
     return this->longitud;
 }
+
 
 /** 
 * Poniendo static, no necesito tener una instancia de una coordenada para llamar a la función.
@@ -26,51 +27,67 @@ const Nat Coordenada::Longitud() const
 */
 static Nat Coordenada::distEuclidea(const Coordenada coord1, const Coordenada coord2) const
 {
-{
+    Nat c1_lat = coord1.latitud;
+    Nat c1_lon = coord1.longitud;
+    Nat c2_lat = coord2.latitud;
+    Nat c2_lon = coord2.longitud;
+    
+    return pow( (c1_lat >= c2_lat) ? (c1_lat - c2_lat) : (c2_lat - c1_lat), 2 ) +
+           pow( (c1_lon >= c2_lon) ? (c1_lon - c2_lon) : (c2_lon - c1_lon), 2 );
+    
+    
+    /*
     int aux = 0;
-    if (coord1.latitud >= coord2.latitud) {
+    
+    if (coord1.latitud >= coord2.latitud) 
+    {
         aux = (coord1.latitud-coord2.latitud)*(coord1.latitud-coord2.latitud);
     }
-    else    {
+    else
+    {
         aux = (coord2.latitud-coord1.latitud)*(coord2.latitud-coord1.latitud);
     }
-    if (coord1.longitud>=coord2.longitud) {
+    
+    if (coord1.longitud>=coord2.longitud)
+    {
          aux = aux +(coord1.longitud-coord2.longitud)*(coord1.longitud-coord2.longitud);
     }
-    else {
+    else
+    {
         aux = aux +(coord2.longitud-coord1.longitud)*(coord2.longitud-coord1.longitud);
     }
     return aux;
+    */
 }
 
-}
 
 Coordenada Coordenada::CoordenadaArriba() const
 {
-return Coordenada (this->latitud+1,this->longitud);
+    return Coordenada (this->latitud+1,this->longitud);
 }
+
 
 Coordenada Coordenada::CoordenadaAbajo() const
 {
-return Coordenada (this->latitud-1,this->longitud);
+    return Coordenada (this->latitud-1,this->longitud);
 }
 
 Coordenada Coordenada::CoordenadaALaDerecha() const
 {
-return Coordenada (this->latitud,this->longitud+1);
+    return Coordenada (this->latitud,this->longitud+1);
 }
 
 Coordenada Coordenada::CoordenadaALaIzquierda() const
 {
-return Coordenada (this->latitud,this->longitud-1);
+    return Coordenada (this->latitud,this->longitud-1);
 }
 
 bool Coordenada::operator==(const Coordenada &otra) const
 {
-    return this->latitud==latitud(otra) && this->longitud==longitud(otra);
+    return this->latitud==Latitud(otra) && this->longitud==Longitud(otra);
 }
 
 bool Coordenada::operator!=(const Coordenada &c1, const Coordenada &c2) const
 {
-    return not(c1 == c2);
+    return !(c1 == c2);
 }
