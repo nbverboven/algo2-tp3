802f062ad01b3f984dc46d04f77439121481b007
Nicolás Berríos Verboven <nbverboven@gmail.com>
2016-11-25 19:38:32 -0300

    Agregué un test para la grilla. Cambié el código de grilla.h para que se lea mejor

--- code/classes/Grilla/grilla.h
+++ code/classes/Grilla/grilla.h
@@ -1,3 +1,6 @@
+#ifndef H_GRILLA
+#define H_GRILLA
+
 #include "../aed2/aed2.h"
 #include <iostream>
 #include <cassert>
@@ -5,111 +8,177 @@
 using namespace aed2;
 using namespace std;
 
+/*
+ * Implementación del módulo Grilla(alpha)
+ */
+
+template<class T>
+class Grilla
+{
+  public:
+
+    Grilla();
+    Grilla(const Grilla<T>& otro);
+
+    Nat CantFilas() const;
+    Nat CantColumnas() const;
+
+    bool EsVacio() const;
+
+    // Notar que esta operación agrega implícitamente una columna.
+    void AgregarFilas(const Nat n,const T& elem);
+
+    // PRE: la grilla tiene al menos una fila.
+    void AgregarColumnas(const Nat n,const T& elem);
+
+    const T& operator() (Nat i, Nat j) const;
+    T& operator() (Nat i, Nat j);
+
+    Grilla<T>& operator=(const Grilla<T>& otro);
+
+  private:
+
+    Vector< Vector<T> > vector_;
+
+};
 
 
 template<class T>
-    class Grilla {
-        private:
-            Vector< Vector<T> > vector_;
-        public:
-            Grilla();
-            Grilla(const Grilla<T>& otro);
-            Grilla<T>& operator=(const Grilla<T>& otro);
-            bool EsVacio() const;
-            void AgregarColumnas(const Nat n,const T& elem);
-            void AgregarFilas(const Nat n,const T& elem);
-            Nat CantFilas() const;
-            Nat CantColumnas() const;
-            const T& operator() (Nat i, Nat j) const;
-            T& operator() (Nat i, Nat j) ;
-            template <class S>
-            friend std::ostream& operator << (std::ostream& os, const Grilla<S> & g);
-            };
-    template<class T>
-    bool operator==(const Grilla<T>&, const Grilla<T>&);
+Grilla<T>::Grilla() 
+{}
 
 
 template<class T>
-Grilla<T>::Grilla() {}
+Grilla<T>::Grilla(const Grilla<T>& otro) 
+    : vector_(otro.vector_)
+{}
+
 
 template<class T>
-Grilla<T>::Grilla(const Grilla<T>& otro) : vector_(otro.vector_){}
+Grilla<T>& Grilla<T>::operator=(const Grilla<T>& otro)
+{
+    vector_ = otro.vector_;
 
-template<class T>
-Grilla<T>& Grilla<T>::operator=(const Grilla<T>& otro){
-    vector_.operator =(otro.vector_);
     return *this;
 }
 
+
 template<class T>
-bool Grilla<T>::EsVacio() const {
-    return vector_.Longitud()==0;
+bool Grilla<T>::EsVacio() const
+{
+    return vector_.Longitud() == 0;
 }
 
+
 template <class T>
-Nat Grilla<T> :: CantColumnas () const{
-    if (vector_.EsVacio()) {
+Nat Grilla<T>::CantColumnas() const
+{
+    if ( vector_.EsVacio() )
+    {
         return 0;
 	}
-	else { 
+	else
+    { 
         return this->vector_[0].Longitud();
 	}
 
 }
+
+
 template <class T>
-Nat Grilla<T> :: CantFilas () const{
+Nat Grilla<T>::CantFilas() const
+{
     return this->vector_.Longitud();
 }
+
+
 template<class T>
-void Grilla<T>::AgregarColumnas(const Nat n,const T& elem) {
+void Grilla<T>::AgregarColumnas(const Nat n, const T& elem)
+{
 	Nat i = 0 ;
-    while ( i < this->CantFilas()){
+
+    while ( i < this->CantFilas() )
+    {
 		Nat j = 0 ;
-		while (j<n) {
+
+		while ( j < n )
+        {
             this->vector_[i].AgregarAtras(elem);
 			j++;
-			}
+		}
+
 		i++;
 	}
 }
 
+
 template<class T>
-void Grilla<T>::AgregarFilas(const Nat n,const T& elem) {
+void Grilla<T>::AgregarFilas(const Nat n,const T& elem)
+{
     Vector<T> nuevo;
-    if (vector_.EsVacio()){
+
+    if ( vector_.EsVacio() )
+    {
       nuevo.AgregarAtras(elem);
     }
-    else{
-        for (Nat p=0; p < this->CantColumnas(); p ++) nuevo.AgregarAtras(elem) ;}
-    for (Nat j = 0; j < n ; j++) this->vector_.AgregarAtras(nuevo);
-	}
+    else
+    {
+        for (Nat p = 0; p < this->CantColumnas(); p ++)
+        {
+            nuevo.AgregarAtras(elem);
+        }
+    }
+
+    for (Nat j = 0; j < n ; j++) 
+    {
+        this->vector_.AgregarAtras(nuevo);
+    }
+
+}
+
+
 template <class T>
-const T& Grilla<T> :: operator() (Nat i, Nat j) const {
+const T& Grilla<T>::operator() (Nat i, Nat j) const
+{
 	assert (i < CantFilas());
 	assert (j < CantColumnas());
+
     return vector_[i][j];
 }
+
+
 template <class T>
-T& Grilla<T> :: operator() (Nat i, Nat j) {
+T& Grilla<T>::operator() (Nat i, Nat j)
+{
     assert (i < CantFilas());
     assert (j < CantColumnas());
+
     return vector_[i][j];
 }
 
 
 template <class T>
-std::ostream& operator << (std::ostream& os, const Grilla<T>& g)
+std::ostream& operator<<(std::ostream& os, const Grilla<T>& g)
 {
   return Mostrar(os, g.vector_, '<', '>');
 }
 
+
 template<class T>
-bool operator==(const Grilla<T>& g, const Grilla<T>& w) {
+bool operator==(const Grilla<T>& g, const Grilla<T>& w)
+{
     bool retval = (g.CantColumnas() == w.CantColumnas() && g.CantFilas() == w.CantFilas());
-    for(Nat i = 0; i < g.CantFilas() && retval; ++i) {
-        for(Nat j = 0; j < g.CantColumnas() && retval; ++j) {
-        retval = g(i,j) == w(i,j);
+
+    for (Nat i = 0; i < g.CantFilas() && retval; ++i)
+    {
+        for (Nat j = 0; j < g.CantColumnas() && retval; ++j)
+        {
+            retval = ( g(i,j) == w(i,j) );
         }
     }
+
     return retval;
 }
+
+
+#endif /* H_GRILLA */
--- code/classes/Grilla/grillatests.cpp
+++ code/classes/Grilla/grillatests.cpp
@@ -7,42 +7,85 @@ using namespace std;
 using namespace aed2;
 
 
-void test_Grilla() {
+void test_Grilla()
+{
 	Grilla<Nat> g;
+
 	ASSERT_EQ(g.CantFilas(),0);
 	ASSERT_EQ(g.CantColumnas(),0);
 	ASSERT_EQ(g.EsVacio(),true)
 }
 
-void test_constructor() {
-	Grilla <Nat> g;
-	g.AgregarFilas(1,1);
+
+void test_constructor()
+{
+	Grilla<Nat> g;
+
+	g.AgregarFilas(1, 1);
+
 	Grilla <Nat> f(g);
-	ASSERT_EQ(g.CantFilas(),1);
-	ASSERT_EQ(g.CantColumnas(),1);
-	ASSERT_EQ(f.CantFilas(),1);
-	ASSERT_EQ(f.CantColumnas(),1);
-	}
-void test_agregarFilas() {
-	Grilla <Nat> g;
-	g.AgregarFilas(3,4);
-	g.AgregarFilas(1,5);
-	ASSERT_EQ(g.CantFilas(),4);
-	ASSERT_EQ(g(3,0),5)
+
+	ASSERT_EQ(g.CantFilas(), 1);
+	ASSERT_EQ(g.CantColumnas(), 1);
+
+	ASSERT_EQ(f.CantFilas(), 1);
+	ASSERT_EQ(f.CantColumnas(), 1);
 }
 
-void test_agregarColumnas() {
-	Grilla <Nat> g;
-	g.AgregarFilas(3,4);
-	g.AgregarFilas(1,5);
-	g.AgregarColumnas(2,0);
-	ASSERT_EQ(g.CantFilas(),4);
-	ASSERT_EQ(g.CantColumnas(),3);
-	Grilla <Nat> f;
+
+void test_agregarFilas()
+{
+	Grilla<Nat> g;
+
+	g.AgregarFilas(3, 4);
+	g.AgregarFilas(1, 5);
+
+	ASSERT_EQ(g.CantFilas(), 4);
+	ASSERT_EQ(g(3,0), 5)
+}
+
+
+void test_agregarColumnas()
+{
+	Grilla<Nat> g;
+
+	g.AgregarFilas(3, 4);
+	g.AgregarFilas(1, 5);
+	g.AgregarColumnas(2, 0);
+
+	ASSERT_EQ(g.CantFilas(), 4);
+	ASSERT_EQ(g.CantColumnas(), 3);
+
+	Grilla<Nat> f;
+
 	f = g;
-	ASSERT_EQ(f==g,true);
+
+	ASSERT_EQ(f == g, true);
 }
 
+
+void test_grilla_de_grillas()
+{
+	Grilla<bool> g;
+
+	g.AgregarFilas(5, false);
+	g.AgregarColumnas(4, false);
+
+	ASSERT_EQ(g.CantFilas(), 5);
+	ASSERT_EQ(g.CantColumnas(), 5);
+
+	Grilla< Grilla<bool> > super_grilla;
+
+	super_grilla.AgregarFilas(5, g);
+	super_grilla.AgregarColumnas(4, g);
+
+	ASSERT_EQ(super_grilla.CantFilas(), 5);
+	ASSERT_EQ(super_grilla.CantColumnas(), 5);
+
+	super_grilla(0, 0)(0, 1) = true;
+
+	ASSERT_EQ(super_grilla(0, 0)(0, 1), true);
+}
 
 
 int main()
@@ -51,7 +94,7 @@ int main()
     RUN_TEST(test_constructor);
     RUN_TEST(test_agregarFilas);
     RUN_TEST(test_agregarColumnas);
-    
+    RUN_TEST(test_grilla_de_grillas);
 
     return 0;
 }
