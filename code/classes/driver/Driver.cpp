e47df2f207edefde1c75dd160c28036dbfc8d6d8
Nicolás Berríos Verboven <nbverboven@gmail.com>
2016-11-26 18:35:05 -0300

    Empecé a instanciar el Driver (creo)

--- code/classes/driver/Driver.cpp
+++ code/classes/driver/Driver.cpp
@@ -4,18 +4,163 @@
 
 Driver::Driver(const Conj< Coordenada > & cs)
 {
-  assert(false);
+	assert(false);
 }
 
+
 Driver::~Driver()
-{
-  assert(false);
-}
+{}
+
 
 void Driver::agregarPokemon(const Pokemon & p, const Coordenada & c)
 {
-  assert(false);
+	driver_juego_.AgregarPokemon(p, c);
 }
 
 
-// TODO: Completar con el resto de las implementaciones
+Jugador Driver::agregarJugador()
+{
+	return driver_juego_.AgregarJugador();
+}
+
+
+void Driver::conectarse(const Jugador & j, const Coordenada & c)
+{
+	driver_juego_.Conectarse(j, c);
+}
+
+
+void Driver::desconectarse(const Jugador & j)
+{
+	driver_juego_.Desconectarse(j);
+}
+
+
+void Driver::moverse(const Jugador & j, const Coordenada & c)
+{
+	driver_juego_.Moverse(j, c);
+}
+
+
+Conj< Coordenada > Driver::mapa() const
+{
+	return driver_juego_. Juego::mapa().Coordenadas();
+}
+
+
+bool Driver::hayCamino(const Coordenada & c1, const Coordenada & c2) const
+{
+	return driver_juego_. Juego::mapa().HayCamino(c1, c2);
+}
+
+
+bool Driver::posExistente(const Coordenada & c) const
+{
+	return driver_juego_. Juego::mapa().PosExistente(c);
+}
+
+
+Conj< Jugador > Driver::jugadores() const
+{
+	Conj<Jugador> res();
+	Conj<Jugador>::const_Iterador it = driver_juego_.Jugadores();
+
+	while ( it.HaySiguiente() )
+	{
+		res.AgregarRapido(it.siguiente());
+		it.Avanzar();
+	}
+
+	return res;
+}
+
+
+bool Driver::estaConectado(const Jugador & j) const
+{
+	return driver_juego_.EstaConectado(j);
+}
+
+
+Nat Driver::sanciones(const Jugador & j) const
+{
+	return driver_juego_.Sanciones(j);
+}
+
+
+Coordenada Driver::posicion(const Jugador & j) const
+{
+	return driver_juego_.Posicion(j);
+}
+
+
+Dicc< Pokemon , Nat > Driver::pokemons(const Jugador & j) const
+{
+	Dicc<Pokemon, Nat> res();
+	
+}
+
+
+Conj< Jugador > Driver::expulsados() const
+{
+	return driver_juego_.Expulsados();
+}
+
+
+Conj< Coordenada > Driver::posConPokemons() const
+{
+	return driver_juego_.PosConPokemon();
+}
+
+
+Pokemon Driver::pokemonEnPos(const Coordenada & c) const
+{
+	return driver_juego_.PokemonEnPos(c);
+}
+
+
+Nat Driver::cantMovimientosParaCaptura(const Coordenada & c) const
+{
+	return driver_juego_.CantMovimientosParaCaptura(c);
+}
+
+
+bool Driver::puedoAgregarPokemon(const Coordenada & c) const
+{
+	return driver_juego_.PuedoAgregarPokemon(c);
+}
+
+
+bool Driver::hayPokemonCercano(const Coordenada & c) const
+{
+	return driver_juego_.HayPokemonCercano(c);
+}
+
+
+Coordenada Driver::posPokemonCercano(const Coordenada & c) const
+{
+	return driver_juego_.PosPokemonCercano(c);
+}
+
+
+Conj<Jugador> Driver::entrenadoresPosibles(const Coordenada & c) const
+{
+	return driver_juego_.EntrenadoresPosibles(c);
+}
+
+
+Nat Driver::indiceRareza(const Pokemon & p) const
+{
+	return driver_juego_.IndiceDeRareza(p);
+}
+
+
+Nat Driver::cantPokemonsTotales() const
+{
+	return driver_juego_.CantPokemonsTotales();
+}
+
+
+Nat Driver::cantMismaEspecie(const Pokemon & p) const
+{
+	return driver_juego_.CantMismaEspecie(p);
+}
